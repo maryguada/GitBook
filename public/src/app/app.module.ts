8753c63283d8dd757d1a05321c69f505b889760c
Howard Chen <howardzhengrui@gmail.com>
2019-09-23 16:43:32 -0700

    preparing to pull

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpService } from './http.service';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard/dashboard.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
-
+const path= require("path");
 
 app.use(express.static( __dirname + '/public/dist/public'));
-
 
 app.all("*", (req, res, next) => {
     res.sendFile(path.resolve("./public/dist/public/index.html"))
