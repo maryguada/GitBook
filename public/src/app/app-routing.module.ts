4802c3652394c1492e85ef4901a57b9122187c21
Cean Tolentino <cean.tolentino@gmail.com>
2019-09-24 11:14:16 -0700

    Edited dashboard, added home as the main page

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component'
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path :'',component:DashboardComponent},
+  {path :'',component:HomeComponent, children: [
+    {path: 'dashboard', component: DashboardComponent},
+    ]},
+  {path: 'login', component: LoginComponent},
+
 ];
 
 @NgModule({
--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpService } from './http.service';
 import { HttpClientModule } from '@angular/common/http';
-import { DashboardComponent } from './dashboard/dashboard.component'
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
