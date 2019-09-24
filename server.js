7d527711354c15b10dc8c2baee98a467d6df6b66
Cean Tolentino <cean.tolentino@gmail.com>
2019-09-24 14:42:03 -0700

    working version. preparing to add login github oauth

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const app = express();
 const path = require('path')
-
+const GitHubStrategy = require('passport-github').Strategy;
+const passport = require('passport')
 app.use(express.static( __dirname + '/public/dist/public'));
+
+passport.use(new GitHubStrategy({
+    clientID: "f238e78c777380efe8db",
+    clientSecret: "7870a58b5c634c133d52dc7b46bfae023f1b1bf1",
+    callbackURL: "http://localhost:8000/user/signin/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile.id)
+    console.log(accessToken)
+    console.log(refreshToken)
+    User.findOrCreate({ githubId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get('/auth/github',
+  passport.authenticate('github'));
+
+app.get('/user/signin/callback', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    console.log(req.body)
+    res.redirect('/');
+  });
 
 app.all("*", (req, res, next) => {
     res.sendFile(path.resolve("./public/dist/public/index.html"))
